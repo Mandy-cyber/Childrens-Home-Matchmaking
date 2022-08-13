22297fcccc8349817b91a8f42d37f45834745917
Mandy-cyber <92461362+PeriodPrideJa@users.noreply.github.com>
2022-08-13 01:55:35 -0500

    fixing typo and changing view name

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'changethislater' #encrypting session data
-    app.config['SQLALCHEMY_DATABSE_URI'] = f"sqlite:///{DB_NAME}" #location of database
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}" #location of database
     db.init_app(app)
 
     from .views import views
@@ -18,11 +18,11 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import Users
+    from .models import User
     create_database(app)
 
     login_manager = LoginManager()
-    login_manager.login_view = "views.choose_a_signup"
+    login_manager.login_view = "views.home"
     login_manager.init_app(app)
     @login_manager.user_loader
     def load_user(id):
