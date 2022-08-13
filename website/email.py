dbde3cceba215c0b946b2961f500384f66b5da09
Mandy-cyber <92461362+PeriodPrideJa@users.noreply.github.com>
2022-08-13 00:57:51 -0500

    new file

--- website/email.py
+++ website/email.py
@@ -8,32 +8,24 @@ cursor = conn.cursor()
 
 
 ## getting 'emails' column from the table
-cursor.execute("SELECT name FROM students")
+cursor.execute("SELECT email FROM email_list")
 
 ## fetching all usernames from the 'cursor' object
 emails = cursor.fetchall()
 
-## function for sending the emails
-# def send_email(message): 
-#     for email in emails:
-#         strippedText = str(email).replace('(','').replace(')','').replace(',','').replace('\"','')
-#         server = smtplib.SMTP_SSL("smtp.gmail.com",465)
-#         server.login("examplw@gmail.com", "password")
-#         server.sendmail(f"example@gmail.com",
-#                         {email},
-#                         {message})
-
-        # server.quit()
-
+# function for sending the emails
 def send_email(message): 
+    for email in emails:
+        strippedText = str(email).replace('(','').replace(')','').replace(',','').replace('\"','')
         server = smtplib.SMTP_SSL("smtp.gmail.com",465)
-        server.login("emailfortesting2345@gmail.com", "orangejuice123!")
-        server.sendmail(f"emailfortesting2345@gmail.com",
-                        "parchiejosh@gmail.com",
-                        {message})
-
+        server.login("emailfortesting2345@gmail.com", "aawu hqdr jrtj kejp")
+        server.sendmail(f"example@gmail.com",
+                        email,
+                        message)
         server.quit()
 
-message = input("Please insert email you would like to send")
+
+message = input("Please insert email you would like to send? ")
 send_email(message)
-    
+
+
