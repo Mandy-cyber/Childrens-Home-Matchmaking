9c612736db3aa13e633f86f55123c2a26d159d93
Mandy-cyber <92461362+PeriodPrideJa@users.noreply.github.com>
2022-08-14 02:00:47 -0500

    editing matchmaking python survey

--- website/matcher.py
+++ website/matcher.py
@@ -7,23 +7,46 @@ conn = sqlite3.connect('match.db')
 cursor = conn.cursor()
 matched_home = []
 selected_budget_list = []
+list_of_matched_homes = []
+id_counter = 0
+id_counter = id_counter + 1
+
+# -----------------------------------------------------------------------------------------------------------------#
+#CREATINGT THE TABLE
+create_table = """CREATE TABLE IF NOT EXISTS
+homes(id INTEGER PRIMARY KEY autoincrement, parish TEXT, name TEXT, type TEXT, size TEXT, budget TEXT)"""
+cursor.execute(create_table)
+
+create_table = """CREATE TABLE IF NOT EXISTS
+matchmaker(id INTEGER PRIMARY KEY autoincrement, parish TEXT, type TEXT, size TEXT, budget TEXT)"""
+cursor.execute(create_table)
+
+
+
+# -----------------------------------------------------------------------------------------------------------------#
+#ALLOWS SO THAT THE ID FOR THE FIRST ROW IS ALWAYS 1
+cursor.execute('''UPDATE matchmaker SET id = ? WHERE id = ?''', ('1', str(id_counter)))
 
 # -----------------------------------------------------------------------------------------------------------------#
 #GETTING THE NECESSARY COLUMNS FROM THE MATCHMAKER TABLES
 cursor.execute("SELECT parish FROM matchmaker")
 parish_matchmaker = cursor.fetchall()
-parish_matchmaker = str(parish_matchmaker).replace('(','').replace(')','').replace('\"','')
+parish_matchmaker = str(parish_matchmaker).replace('(','').replace(')','').replace('\"','').replace('\'','').replace(',','').replace('[','').replace(']','')
+
+cursor.execute("SELECT size FROM matchmaker")
+size_matchmaker = cursor.fetchall()
+size_matchmaker = str(size_matchmaker).replace('(','').replace(')','').replace('\"','').replace('\'','').replace(',','').replace('[','').replace(']','')
 
 cursor.execute("SELECT budget FROM matchmaker")
 budget_matchmaker = cursor.fetchall()
-budget_matchmaker = str(budget_matchmaker).replace('(','').replace(')','').replace('\"','')
+budget_matchmaker = str(budget_matchmaker).replace('(','').replace(')','').replace('\"','').replace('\'','').replace(',','').replace('[','').replace(']','')
 budget_matchmaker = int(budget_matchmaker)
 max_budget_matchmaker = budget_matchmaker + 10000
 min_budget_matchmaker = budget_matchmaker - 10000
 
 cursor.execute("SELECT type FROM matchmaker")
 type_matchmaker = cursor.fetchall()
-type_matchmaker = str(type_matchmaker).replace('(','').replace(')','').replace('\"','')
+type_matchmaker = str(type_matchmaker).replace('(','').replace(')','').replace('\"','').replace('\'','').replace(',','').replace('[','').replace(']','')
 
 # -----------------------------------------------------------------------------------------------------------------#
 # CLEARING THE DATABASE SO THERE WILL ONLY BE ONE ROW OF DATA WHEN THE PROGRAM IS RAN AGAIN
@@ -35,7 +58,7 @@ except:
 
 # -----------------------------------------------------------------------------------------------------------------#
 # GETTING THE ROWS FROM THE HOME TABLE THAT HAVE THE SAME PARISH AND HOME TYPE AS SELECTED BY THE USER       
-cursor.execute('''SELECT id FROM homes WHERE parish = ? AND type = ?''', (parish_matchmaker, type_matchmaker))
+cursor.execute('''SELECT id FROM homes WHERE parish = ? AND type = ? AND size = ?''', (parish_matchmaker, type_matchmaker, size_matchmaker))
 selected_home = cursor.fetchall()
 
 # -----------------------------------------------------------------------------------------------------------------#
@@ -47,22 +70,32 @@ for x in selected_home:
 conn.commit()
 
 for budget in selected_budget_list:
-    budget = str(budget_matchmaker).replace('(','').replace(')','').replace('\"','')
+    str_budget = budget
+    budget = str(budget_matchmaker).replace('(','').replace(')','').replace('\"','').replace('\'','').replace(',','').replace('[','').replace(']','')
     budget = int(budget)
 
 # -----------------------------------------------------------------------------------------------------------------#
 # SELECTS THE BUDGETS THAT ARE EITHER $10,000 MORE OR LESS THAN THE BUDGET GIVEN BY THE USER AND THEN FINDS THE ID OF THE BUDGETS 
     if budget in range(min_budget_matchmaker, max_budget_matchmaker):
-        budget_index = selected_budget_list.index(budget)
+        budget_index = selected_budget_list.index(str_budget)
         matched_budget_number = budget_index + 1
         matched_home.append(matched_budget_number)
         
 # -----------------------------------------------------------------------------------------------------------------#
 # ID IS USED TO FIND THE NAME OF THE MATCHED HOMES 
         for x in matched_home:
-            cursor.execute('''SELECT name FROM homes WHERE id = ?''', (x))
-            name_of_matched_homes = cursor.fetchall
+            cursor.execute(f"SELECT name FROM homes WHERE id = {x};")
+            name_of_matched_homes = cursor.fetchall()
+            for x in name_of_matched_homes:
+                matched_home_stripped = str(budget_matchmaker).replace('(','').replace(')','').replace('\"','').replace('\'','').replace(',','').replace('[','').replace(']','')
+                list_of_matched_homes.append(matched_home_stripped) 
         conn.commit()
     
     else:
         pass
+
+
+print(list_of_matched_homes)
+
+
+
